b5908597ffb271e3051277ac70d2cbf144c08825
hunjuly <hunjuly@gmail.com>
2023-09-14 12:46:50 +0000

    .

--- src/AvatarEditor/configs/body.ts
+++ src/AvatarEditor/configs/body.ts
@@ -4,5 +4,9 @@ export const body: Resource[] = [
     {
         id: '1',
         fileUrl: '/parts/body/body1-1.glb'
+    },
+    {
+        id: '2',
+        fileUrl: '/parts/body/body1-2.glb'
     }
 ]
