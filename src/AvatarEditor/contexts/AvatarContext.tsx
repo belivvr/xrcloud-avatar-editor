c4209ea3f0a49fbf7f627fad528463bd394f3c45
hunjuly <hunjuly@gmail.com>
2023-09-20 10:00:04 +0000

    .

--- src/AvatarEditor/contexts/AvatarContext.tsx
+++ src/AvatarEditor/contexts/AvatarContext.tsx
@@ -1,41 +1,54 @@
 import { useGLTF } from '@react-three/drei'
 import React, { ReactNode, useContext, useEffect, useRef, useState } from 'react'
 import { Group } from 'three'
-import { femaleAvatarBlueprint, femaleAvatarInstance, loopThroughBlueprint } from '../blueprints/femaleAvatar'
-import { AvatarContextValue, AvatarInstance } from './AvatarContext.type'
+import { allAvatarBlueprints, loopThroughBlueprint, makeAvatarInstance } from '../blueprints'
+import { AvatarBlueprint, AvatarContextValue, AvatarInstance } from './AvatarContext.type'
+
+파츠를 export 할 때 animations 체크 안 하면 크기가 줄어드는 것 같다
+아바다 교체하면 애니메이션이 제대로 안 되는 것 같다.
 
 export const AvatarContext = React.createContext({} as AvatarContextValue)
 
 export function AvatarProvider({ children }: { children: ReactNode }) {
     const rootRef = useRef<Group>(null)
-    const [avatarInstance, setAvatarInstance] = useState<AvatarInstance>(femaleAvatarInstance)
+    const [avatarBlueprint, setAvatarBlueprint] = useState<AvatarBlueprint>(allAvatarBlueprints[1])
+    const [avatarInstance, setAvatarInstance] = useState<AvatarInstance>(makeAvatarInstance(avatarBlueprint))
 
     useEffect(() => {
-        loopThroughBlueprint(femaleAvatarBlueprint, (item) => {
+        loopThroughBlueprint(avatarBlueprint, (item) => {
             if (item.fileUrl) {
                 window.requestIdleCallback(() => {
                     useGLTF.preload(item.fileUrl)
                 })
             }
         })
-    }, [avatarInstance.skeleton])
+    }, [avatarBlueprint, avatarInstance.skeleton])
 
     const setBody = (bodyNo: number) => {
         const updatedFemaleAvatarInstance = {
             ...avatarInstance,
             parts: {
                 ...avatarInstance.parts,
-                Body: femaleAvatarBlueprint.bodies[bodyNo]
+                Body: avatarBlueprint.bodies[bodyNo]
             }
         }
 
         setAvatarInstance(updatedFemaleAvatarInstance)
     }
 
+    const setSkeleton = (skeletonNo: number) => {
+        const blueprint = allAvatarBlueprints[skeletonNo]
+        setAvatarBlueprint(blueprint)
+
+        const instance = makeAvatarInstance(blueprint)
+        setAvatarInstance(instance)
+    }
+
     const context = {
         avatarInstance,
         rootRef,
-        setBody
+        setBody,
+        setSkeleton
     }
 
     return <AvatarContext.Provider value={context}>{children}</AvatarContext.Provider>
